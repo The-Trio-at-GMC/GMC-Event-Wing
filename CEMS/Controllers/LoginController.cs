d609caa912a8a2c165c781550ca2230e560f2cb5
Dipa <tamangdipa77@gmail.com>
2026-03-17 22:51:08 +0545

    feat: add splash screen

---
+++ CEMS/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult SplashScreen()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
