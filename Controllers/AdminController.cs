d246cd073551813b6668f5f004c3d0dfec769345
Bhawana Rai <bhawanasambawa26@gmail.com>
2026-03-20 13:20:10 +0545

    Added Admin Dashboard UI

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEMS.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult SplashScreen()
+        {
+            return View();
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult SendResetCode()
+        {
+            return RedirectToAction("VerifyResetCode", "Login");
+        }
+
+        public IActionResult VerifyResetCode()
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
