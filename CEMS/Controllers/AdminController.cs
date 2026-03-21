cb7055580af57a930791929e1eee616c85c80237
Bhawana Rai <bhawanasambawa26@gmail.com>
2026-03-21 12:01:12 +0545

    feat: add admin dashboard

---
+++ CEMS/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CEMS.Controllers;
+
+public class AdminController: Controller
+{
+    public IActionResult AdminDashboard()
+    {
+        return View();
+    }
+}
